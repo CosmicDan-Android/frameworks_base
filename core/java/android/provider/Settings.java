2ceb27df1b125f79e23486007c8ad4561eab41c2
Lars Greiss <kufikugel@googlemail.com>
2013-12-01 14:14:38 +0100

    Bring back less notification sounds [1/2]
    abc: make it work together with screen On suppression feature
    and default to 30seconds threshold
    
    Add back the 4.3 feature like it was.
    User can setup how often notification sounds
    for an app come through so that it does
    not annoy too much.
    
    Change-Id: I3d7b2ef2e2ff99b7c23880626e0268ec4c5d8543

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4374,6 +4374,12 @@ public final class Settings {
          */
         public static final String NOTIFICATION_SOUND_VIB_SCREEN_ON = "notification_sound_vib_screen_on";
 
+        /**
+         * Whether to mute annoying notifications
+         * @hide
+         */
+        public static final String MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD = "mute_annoying_notifications_threshold";
+
         /** End of ABC system settings**/
 
         /**
