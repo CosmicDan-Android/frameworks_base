dffeb372408c2f6db1a2fa33a4b3d4f9b1dcf187
ezio84 <brabus84@gmail.com>
2018-06-16 16:57:19 +0200

    Fix screenshot not saved when appending appname with some languages
    like Virgin Islands English when taking a screenshot of the Settings app
    
    todo: find a less noob way, if possible, to handle this
    
    Change-Id: Ic04f66f5813b9597c96835d15c74509c93530a5c

--- core/java/com/android/internal/util/du/Utils.java
+++ core/java/com/android/internal/util/du/Utils.java
@@ -17,8 +17,10 @@
 package com.android.internal.util.du;
 
 import android.Manifest;
+import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
@@ -43,6 +45,7 @@ import android.view.WindowManagerGlobal;
 import com.android.internal.R;
 import com.android.internal.statusbar.IStatusBarService;
 
+import java.util.List;
 import java.util.Locale;
 
 public class Utils {
@@ -208,5 +211,21 @@ public class Utils {
                         InputManager.INJECT_INPUT_EVENT_MODE_ASYNC);
             }
         }, 20);
+    }
+
+    public static ActivityInfo getRunningActivityInfo(Context context) {
+        final ActivityManager am = (ActivityManager) context
+                .getSystemService(Context.ACTIVITY_SERVICE);
+        final PackageManager pm = context.getPackageManager();
+
+        List<ActivityManager.RunningTaskInfo> tasks = am.getRunningTasks(1);
+        if (tasks != null && !tasks.isEmpty()) {
+            ActivityManager.RunningTaskInfo top = tasks.get(0);
+            try {
+                return pm.getActivityInfo(top.topActivity, 0);
+            } catch (PackageManager.NameNotFoundException e) {
+            }
+        }
+        return null;
     }
 }
