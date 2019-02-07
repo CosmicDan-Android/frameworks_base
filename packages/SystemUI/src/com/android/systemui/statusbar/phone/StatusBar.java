d0d755a06935aa9dd435ef028168fee40656d9ec
ezio84 <brabus84@gmail.com>
2019-02-07 21:07:17 +0100

    Ambient music: show on lockscreen only for Google's Now Playing
    or for local media player if its notification is hidden in lockscreen
    by user choice or all lockscreen notifications are hidden

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -4134,6 +4134,12 @@ public class StatusBar extends SystemUI implements DemoMode,
         mKeyguardIndicationController.setDozing(mDozing);
         mNotificationPanel.setDozing(mDozing, animate);
         updateQsExpansionEnabled();
+
+        if (isAmbientContainerAvailable()) {
+            ((AmbientIndicationContainer)mAmbientIndicationContainer)
+                    .updateDozingState(mDozing);
+        }
+
         Trace.endSection();
     }
 
