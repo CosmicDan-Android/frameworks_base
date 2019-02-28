b80361ddf709c16ce87d545f5acebd8f91520d17
Dil3mm4 <dil3mm4.dev@gmail.com>
2019-02-28 15:27:28 +0000

    Revert "Seekbars and volumebar overlays to follow design consistency on UI corners change"
    This reverts commit d6ef31d4fc67e9e7e0437a9d25df04ab6f7fc638.

--- core/java/com/android/internal/statusbar/ThemeUtils.java
+++ core/java/com/android/internal/statusbar/ThemeUtils.java
@@ -30,15 +30,11 @@ public class ThemeUtils {
         private static final String[] ROUNDIERUITHEME  = {
             "org.descendant.UI.android.roundier", 
             "org.descendant.UI.system.roundier",        
-            "org.descendant.seekbars.roundier",
-            "org.descendant.volumebar.roundier"
         };
 
         private static final String[] SQUAREUITHEME  = {
             "org.descendant.UI.android.square",
             "org.descendant.UI.system.square",
-            "org.descendant.seekbars.square",
-            "org.descendant.volumebar.square",
         };
 
         private static final String[] DESCENDANTICONTHEMES = {
