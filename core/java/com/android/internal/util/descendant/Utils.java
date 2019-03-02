300ed15afa04ed09607b1cf9949724d33069e61b
Dil3mm4 <dil3mm4.dev@gmail.com>
2019-03-02 18:52:40 +0000

    Revert "ActionUtils: Reverse actions for QS and notification panel"
    This reverts commit b0e0fe5753eeb7d37e6b6c70d1a0eaa6724a2e93.

--- core/java/com/android/internal/util/descendant/Utils.java
+++ core/java/com/android/internal/util/descendant/Utils.java
@@ -334,7 +334,7 @@ public class Utils {
         IStatusBarService service = getStatusBarService();
         if (service != null) {
             try {
-                service.togglePanel();
+                service.expandNotificationsPanel();
             } catch (RemoteException e) {
                 // do nothing.
             }
@@ -346,7 +346,7 @@ public class Utils {
         IStatusBarService service = getStatusBarService();
         if (service != null) {
             try {
-                service.toggleSettingsPanel(null);
+                service.expandSettingsPanel(null);
             } catch (RemoteException e) {
                 // do nothing.
             }
