c39598727fcc14c0dcf1c611d39b9078005d6577
Dil3mm4 <dil3mm4.dev@gmail.com>
2019-03-21 03:24:44 +0000

    Revert "Extended audio panel [1/2]"
    This reverts commit 711717cac673affebfd621a25b9e882cdb8df431.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4490,24 +4490,6 @@ public final class Settings {
         /** @hide */
         public static final String THUMB_UI = "thumb_ui";
 
-        /** @hide */
-        public static final String AUDIO_PANEL_VIEW_MEDIA = "audio_panel_view_media";
-
-        /** @hide */
-        public static final String AUDIO_PANEL_VIEW_RINGER = "audio_panel_view_ringer";
-
-        /** @hide */
-        public static final String AUDIO_PANEL_VIEW_NOTIFICATION = "audio_panel_view_notification";
-
-        /** @hide */
-        public static final String AUDIO_PANEL_VIEW_ALARM = "audio_panel_view_alarm";
-
-        /** @hide */
-        public static final String AUDIO_PANEL_VIEW_VOICE = "audio_panel_view_voice";
-
-        /** @hide */
-        public static final String AUDIO_PANEL_VIEW_BT_SCO = "audio_panel_view_bt_sco";
-
         /** End of Descendant system settings**/
         
         /**
